2e451369cccbff869d619ccba9a984ad4b6b0c35
sithu <sithu.bumpercrop@gmail.com>
2025-08-04 23:59:49 +1200

    update sql file

--- sql/create_patients_dim.sql
+++ sql/create_patients_dim.sql
@@ -1,12 +1,12 @@
 CREATE OR REPLACE TABLE `missed_appointments.patients_dim` AS
 SELECT
-  DISTINCT
   patient_id,
-  gender,
-  age,
-  gov_aid,
-  hypertension,
-  diabetes,
-  alcoholism,
-  handicap
-FROM `missed_appointments.appointments_data_cleaned`;
+  ANY_VALUE(gender) AS gender,
+  ANY_VALUE(age) AS age,
+  ANY_VALUE(gov_aid) AS gov_aid,
+  ANY_VALUE(hypertension) AS hypertension,
+  ANY_VALUE(diabetes) AS diabetes,
+  ANY_VALUE(alcoholism) AS alcoholism,
+  ANY_VALUE(handicap) AS handicap
+FROM `missed_appointments.appointments_data_cleaned`
+GROUP BY patient_id;
